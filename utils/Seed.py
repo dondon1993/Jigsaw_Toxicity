9b0b49fc5d2db7cfc79265b14294064234dbb3dd
Don <48937830+dondon1993@users.noreply.github.com>
2020-06-24 00:56:23 -0400

    Update Seed.py

--- utils/Seed.py
+++ utils/Seed.py
@@ -1,25 +1,11 @@
-
-# coding: utf-8
-
-# In[11]:
-
-
 import random
 import numpy as np
 import torch
 import pickle
 
-
-# In[12]:
-
-
 class seed_config:
     def __init__(self, SEED):
         self.seed = SEED
-
-
-# In[13]:
-
 
 def seed_everything(SEED):
     np.random.seed(SEED)
@@ -30,4 +16,3 @@ def seed_everything(SEED):
         l = pickle.load(handle)
 
     torch.cuda.set_rng_state(l)
-
